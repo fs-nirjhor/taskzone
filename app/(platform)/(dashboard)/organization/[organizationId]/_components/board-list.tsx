c8966b8248fffc1bb374909d434a9d1af401be8b
fs-nirjhor <fsnirjhor2002@gmail.com>
2024-08-10 10:15:07 +0600

    popoverclose

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,3 +1,4 @@
+import { FormPopover } from "@/components/form/form-popover";
 import { Hint } from "@/components/hint";
 import { HelpCircle, User2 } from "lucide-react";
 
@@ -8,16 +9,21 @@ export const BoardList = () => {
         <User2 className="size-6" /> <span>Your Boards</span>
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-        <div className="aspect-video relative size-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition">
-          <p className="text-sm">Create New Board</p>
-          <span className="text-sm">5 Remaining</span>
-          <Hint
-            sideOffset={40}
-            description="Free workspace can have up to 5 open boards. for unlimited boards upgrade this workspace"
+        <FormPopover sideOffset={10} side="right">
+          <div
+            role="button"
+            className="aspect-video relative size-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
           >
-            <HelpCircle className="absolute bottom-2 right-2 size-3.5" />
-          </Hint>
-        </div>
+            <p className="text-sm">Create New Board</p>
+            <span className="text-sm">5 Remaining</span>
+            <Hint
+              sideOffset={40}
+              description="Free workspace can have up to 5 open boards. for unlimited boards upgrade this workspace"
+            >
+              <HelpCircle className="absolute bottom-2 right-2 size-3.5" />
+            </Hint>
+          </div>
+        </FormPopover>
       </div>
     </div>
   );
--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -1,1 +1,47 @@
- 
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { X } from "lucide-react";
+
+interface FormPopoverProps {
+  children: React.ReactNode;
+  side?: "left" | "right" | "top" | "bottom";
+  align?: "start" | "center" | "end";
+  sideOffset: number;
+}
+
+export const FormPopover = ({
+  children,
+  side = "bottom",
+  align,
+  sideOffset,
+}: FormPopoverProps) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>{children}</PopoverTrigger>
+      <PopoverContent
+        align={align}
+        side={side}
+        sideOffset={sideOffset}
+        className="w-80 pt-3"
+      >
+        <div className="text-sm font-medium text-center text-neutral-600 pb-4">
+          Create Board
+        </div>
+        <PopoverClose asChild>
+          <Button
+            className="size-auto p-2 absolute top-2 right-2 text-neutral-600"
+            variant="ghost"
+            size="sm"
+          >
+            <X className="size-4" />
+          </Button>
+        </PopoverClose>
+      </PopoverContent>
+    </Popover>
+  );
+};
