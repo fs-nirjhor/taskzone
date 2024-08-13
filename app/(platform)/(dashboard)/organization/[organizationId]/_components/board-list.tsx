a4798173308d617f7a24de456c5019bd965aaedb
fs-nirjhor <fsnirjhor2002@gmail.com>
2024-08-13 22:15:53 +0600

    :boom: BoardList.Skeleton

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,30 +1,74 @@
 import { FormPopover } from "@/components/form/form-popover";
 import { Hint } from "@/components/hint";
+import { Skeleton } from "@/components/ui/skeleton";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
 import { HelpCircle, User2 } from "lucide-react";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export const BoardList = () => {
+export const BoardList = async () => {
+  const { orgId } = auth();
+  if (!orgId) {
+    return redirect("/select-organization");
+  }
+  const boards = await db.board.findMany({
+    where: { orgId },
+    orderBy: { createdAt: "desc" },
+  });
   return (
-      <div className="space-y-4">
-        <div className="flex items-center gap-2 font-semibold text-lg text-neutral-700">
-          <User2 className="size-6" /> <span>Your Boards</span>
-        </div>
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-          <FormPopover sideOffset={10} side="right">
-            <div
-              role="button"
-              className="aspect-video relative size-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
-            >
-              <p className="text-sm">Create New Board</p>
-              <span className="text-sm">5 Remaining</span>
-              <Hint
-                sideOffset={40}
-                description="Free workspace can have up to 5 open boards. for unlimited boards upgrade this workspace"
-              >
-                <HelpCircle className="absolute bottom-2 right-2 size-3.5" />
-              </Hint>
-            </div>
-          </FormPopover>
-        </div>
+    <div className="space-y-4">
+      <div className="flex items-center gap-2 font-semibold text-lg text-neutral-700">
+        <User2 className="size-6" /> <span>Your Boards</span>
       </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+        {boards.map((board) => (
+          <Link
+            key={board.id}
+            href={`/board/${board.id}`}
+            className="group relative aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm size-full overflow-hidden"
+            style={{ backgroundImage: `url(${board.imageThumbUrl})` }}
+          >
+            <div className="absolute inset-0 bg-black/30 group-hover:bg-black/40 transition p-2">
+              <p className="relative font-semibold text-white">{board.title}</p>
+            </div>
+          </Link>
+        ))}
+        <FormPopover sideOffset={10} side="right">
+          <div
+            role="button"
+            className="aspect-video relative size-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
+          >
+            <p className="text-sm">Create New Board</p>
+            <span className="text-sm">5 Remaining</span>
+            <Hint
+              sideOffset={40}
+              description="Free workspace can have up to 5 open boards. for unlimited boards upgrade this workspace"
+            >
+              <HelpCircle className="absolute bottom-2 right-2 size-3.5" />
+            </Hint>
+          </div>
+        </FormPopover>
+      </div>
+    </div>
+  );
+};
+
+BoardList.SkeletonItem = function BoardListSkeletonItem() {
+  return <Skeleton className="size-full aspect-video" />;
+};
+
+BoardList.Skeleton = function BoardListSkeleton() {
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+      <BoardList.SkeletonItem />
+      <BoardList.SkeletonItem />
+      <BoardList.SkeletonItem />
+      <BoardList.SkeletonItem />
+      <BoardList.SkeletonItem />
+      <BoardList.SkeletonItem />
+      <BoardList.SkeletonItem />
+      <BoardList.SkeletonItem />
+    </div>
   );
 };
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,6 +1,7 @@
 import { Separator } from "@/components/ui/separator";
 import { BoardList } from "./_components/board-list";
 import { Info } from "./_components/info";
+import { Suspense } from "react";
 
 export default async function OrganizationIdPage({
   params: { organizationId },
@@ -8,12 +9,14 @@ export default async function OrganizationIdPage({
   params: { organizationId: string };
 }) {
   return (
-      <section className="w-full mb-20">
-        <Info />
-        <Separator className="my-4"/>
-        <div className="px-4 md:px-4">
-        <BoardList />
-        </div>
-      </section>
+    <section className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-4 md:px-4">
+        <Suspense fallback={<BoardList.Skeleton />} >
+          <BoardList />
+        </Suspense>
+      </div>
+    </section>
   );
 }
