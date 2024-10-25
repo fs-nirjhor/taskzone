0920f4cd0966d26dfc7bf98ef82aeaa2baeeb277
fs-nirjhor <fsnirjhor2002@gmail.com>
2024-10-25 19:45:26 +0600

    💄 Card Header Skeleton

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -5,6 +5,7 @@ import { useCardModal } from "@/hooks/use-card-modal";
 import { fetcher } from "@/lib/fetcher";
 import { CardWithList } from "@/types";
 import { useQuery } from "@tanstack/react-query";
+import { Header } from "./header";
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id);
@@ -18,7 +19,9 @@ export const CardModal = () => {
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent>{cardData?.title}</DialogContent>
+      <DialogContent>
+        {!cardData ? <Header.Skeleton /> : <Header data={cardData} />}
+      </DialogContent>
     </Dialog>
   );
 };
